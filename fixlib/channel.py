4930e5341dc50130e66d6822163420e947dd2e5c
Dirkjan Ochtman <djc.ochtman@kentyde.com>
2010-03-08 16:13:47 +0100

    Properly decode JSON messages coming in on the side channel.
    This is begging for some abstraction shared by all JSON users.

--- fixlib/channel.py
+++ fixlib/channel.py
@@ -1,11 +1,25 @@
+from datetime import datetime, date
 from collections import deque
+
 import asyncore
+import copy
+import fix42
 
 try:
 	import simplejson as json
 except ImportError:
 	import json
 
+def ddecode(s):
+	return datetime.strptime(s, '%Y-%m-%d %H:%M:%S').date()
+
+def dtdecode(s):
+	return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+
+TYPES = {
+	date: (str, ddecode),
+	datetime: (lambda s: str(s)[:19], dtdecode),
+}
 
 class ChannelServer(asyncore.dispatcher):
 	
@@ -25,13 +39,30 @@ class SideChannel(asyncore.dispatcher):
 		self.dest = dest
 		self.buffer = None
 	
+	def _decode(self, msg):
+		new = {}
+		for k, v in msg.iteritems():
+			
+			if k.startswith('_'):
+				continue
+			
+			if k in fix42.REPEAT:
+				msg[k] = [self._decode(i) for i in v]
+			
+			codec = TYPES.get(fix42.WTAGS[k][1])
+			new[k] = v
+			if codec is not None:
+				new[k] = codec[1](v)
+		
+		return new
+	
 	def handle_close(self):
 		self.close()
 	
 	def handle_read(self):
 		raw = self.recv(8192)
 		if raw:
-			msg = json.loads(raw)
+			msg = self._decode(json.loads(raw))
 			self.dest.queue(msg)
 			self.buffer = {'result': 'done'}
 	
